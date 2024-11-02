00d8afae601d7c589ab498063d792c673f47cba2
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-11-02 14:43:12 +0800

    splash scene

---
+++ Splash_AnimationPlayer.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+using System;
+
+public partial class Splash_AnimationPlayer : AnimationPlayer
+{
+    private void _on_animation_finished(StringName anim_name)
+    {
+        GD.Print("Splash Animation Finished");
+        GetTree().ChangeSceneToFile("res://start.tscn");
+    }
+}
